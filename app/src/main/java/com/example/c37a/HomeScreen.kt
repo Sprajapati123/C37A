08a5a2b07d618574f5baca6ad4a482f25faaa65f
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-02 10:38:26 +0545

    lazy column

--- app/src/main/java/com/example/c37a/HomeScreen.kt
+++ app/src/main/java/com/example/c37a/HomeScreen.kt
@@ -1,17 +1,98 @@
 package com.example.c37a
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 @Composable
-fun HomeScreen(){
-    Column(
-        modifier = Modifier.fillMaxSize().background(Color.Yellow)
+fun HomeScreen() {
+    val images = listOf(
+        R.drawable.face,
+        R.drawable.bettafish,
+        R.drawable.dog,
+        R.drawable.bird,
+        R.drawable.hamster,
+        R.drawable.goldfish,
+        R.drawable.guineapig,
+    )
+
+    val names = listOf(
+        "Facebook",
+        "Bettafish",
+        "Dog",
+        "Bird",
+        "Hamster",
+        "Goldfish",
+        "GuineaPig",
+    )
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
     ) {
-        Text("Home screen")
+        item {
+            Text("Welcome")
+            Text("Good Morning")
+        }
+        item {
+            LazyRow {
+                items(images.size) { index ->
+                    Column {
+                        Image(
+                            painter = painterResource(images[index]),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(70.dp)
+                                .padding(end = 10.dp)
+                        )
+                        Text(names[index])
+                    }
+                }
+            }
+        }
+
+       item {
+           Text("Featured Products")
+           LazyVerticalGrid(
+               columns = GridCells.Fixed(3),
+               modifier = Modifier.height(500.dp)
+           ) {
+              items(images.size){index->
+                  Image(
+                      painter = painterResource(images[index]),
+                      contentDescription = null,
+                      modifier = Modifier
+                          .size(70.dp)
+                          .padding(end = 10.dp)
+                  )
+           }
+       }
+
+
+
     }
+    }
+}
+
+@Preview
+@Composable
+fun HomePre() {
+    HomeScreen()
 }
